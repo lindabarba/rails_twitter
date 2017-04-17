100219161c752a16e80748eedcacf4c1d52c3b87
Linda Barba <lindabarba13@gmail.com>
2017-04-17 10:00:41 -0700

    add seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.tweets)
-
 
 Tweet.destroy_all
 
 tweets = Tweet.create([
-    {handle: "Hotdog", content: "Some stuff said here like meow."},
-    {handle: "Mrs. Pickles", content: "Some stuff said here like meow."}
+    {handle: "Hotdog", content: "Meow, feed me."},
+    {handle: "Mrs. Pickles", content: "Meow, pet me."}
 ])
